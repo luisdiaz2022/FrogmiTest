255db25a3055631c5a4373d82a346f4c77e3f235
Luis <luisedgucv@gmail.com>
2024-04-11 11:46:34 -0500

    migrated the filter and data normalizers to the serialziers

--- sismic_api/sismic_data/serializers.py
+++ sismic_api/sismic_data/serializers.py
@@ -1,4 +1,13 @@
+"""Sismic_data Serializer"""
+
+# Utilities
+from decimal import Decimal, ROUND_HALF_UP
+from django.utils import timezone
+
+# Django REST Framework
 from rest_framework import serializers
+
+# Model
 from .models import Feature
 
 class FeatureSerializer(serializers.ModelSerializer):
@@ -16,3 +25,23 @@ class FeatureSerializer(serializers.ModelSerializer):
             'longitude', 
             'latitude'
         ]
+
+    def create(self, validated_data):
+
+        # Convert the time provided by GeopJson into a DateTime field
+        timestamp = validated_data['time'] / 1000  # Convertir milisegundos a segundos
+        formatted_time = timezone.datetime.fromtimestamp(timestamp, tz=timezone.utc)
+        validated_data['time'] = formatted_time
+
+        # Take the decimal provided by GeopJson and round them up into the max amount allowed
+        validated_data['longitude'] = Decimal(validated_data['longitude']).quantize(Decimal('0.000001'), rounding=ROUND_HALF_UP)
+        validated_data['latitude'] = Decimal(validated_data['latitude']).quantize(Decimal('0.000001'), rounding=ROUND_HALF_UP)
+        validated_data['magnitude'] = Decimal(validated_data['magnitude']).quantize(Decimal('0.00001'), rounding=ROUND_HALF_UP)
+
+        # Verify if the Feature already exists # mover a serializer
+        feature, created = Feature.objects.update_or_create(
+            external_id=validated_data['external_id'],
+            defaults=validated_data
+        )
+
+        return feature
--- sismic_api/sismic_data/views/sismic_data.py
+++ sismic_api/sismic_data/views/sismic_data.py
@@ -1,9 +1,5 @@
 """Sismic_data views."""
 
-# Utilities
-from datetime import datetime
-from decimal import Decimal, ROUND_HALF_UP
-
 # Django REST Framework
 from rest_framework.response import Response
 from rest_framework import viewsets, mixins
@@ -21,6 +17,8 @@ class FeatureListViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewset
     
     """Feature view set."""
 
+    serializer_class = FeatureSerializer
+
     def save_features(self, request, *args, **kwargs):
         """ In this method we are getting the data from GeoJson,
         and saving it on our BD
@@ -30,40 +28,17 @@ class FeatureListViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewset
         if sismic_data: # cambiar a try catch -- debemos mostrar cual error esta pasando
             for feature in sismic_data: # Opcional Bulk_create permite trabajar performante con grandes datos
 
-                # Convert the time provided by GeopJson into a DateTime field
-                timestamp = feature['properties']['time'] / 1000  # Convertir milisegundos a segundos
-                formatted_time = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-
-                # Take the decimal provided by GeopJson and round them up into the max amount allowed
-                longitude = Decimal(feature['geometry']['coordinates'][0]).quantize(Decimal('0.000001'), rounding=ROUND_HALF_UP) #get ATTR revisar para obtener el valor del diccionario
-                latitude = Decimal(feature['geometry']['coordinates'][1]).quantize(Decimal('0.000001'), rounding=ROUND_HALF_UP) #ceil function recommend by cabrera to round up
-                magnitude = Decimal(feature['properties']['mag'],).quantize(Decimal('0.00001'), rounding=ROUND_HALF_UP)
-
-                # Verify if the Feature already exists # mover a serializer
-                if Feature.objects.filter( # update or create method - mas performante
-                    external_id=feature.get('external_id', ''),
-                    magnitude=magnitude,
-                    place=feature['properties']['place'],
-                    time=formatted_time,
-                    tsunami=feature['properties']['tsunami'],
-                    mag_type=feature['properties']['magType'],
-                    title=feature['properties']['title'],
-                    longitude=longitude,
-                    latitude=latitude
-                ).exists():
-                    continue  # if already exists, it moves to the next feature
-
                 # Send the Feature data to serializer before saving it to the database
                 serializer = FeatureSerializer(data={
                     'external_id': feature.get('external_id', ''),
                     'magnitude': feature['properties']['mag'],
                     'place': feature['properties']['place'],
-                    'time': formatted_time,
+                    'time': feature['properties']['time'],
                     'tsunami': feature['properties']['tsunami'],
                     'mag_type': feature['properties']['magType'],
                     'title': feature['properties']['title'],
-                    'longitude': longitude,
-                    'latitude': latitude
+                    'longitude': feature['geometry']['coordinates'][0],
+                    'latitude': feature['geometry']['coordinates'][1]
                 })
                 if serializer.is_valid():
                     serializer.save()
